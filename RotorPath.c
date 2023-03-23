2987b4ae91b0ac822019488b961fde156fb51196
TylerBruno0 <127354973+TylerBruno0@users.noreply.github.com>
2023-03-23 09:47:17 -0700

    Update RotorPath.c

--- RotorPath.c
+++ RotorPath.c
@@ -35,8 +35,10 @@ void Rotor1Spin() // Spins Rotor 1 once
             RotorPath[0][X] = RotorPath[0][X-1];
         }
     RotorPath[0][0] = store;
+    
     for (X = 0; X < 26; X++)
         printf("%s", RotorPath[0][X]);
+        printf("\n");
 }
 void Rotor2Spin() // Spins Rotor 2 once
 {
@@ -48,6 +50,10 @@ void Rotor2Spin() // Spins Rotor 2 once
             RotorPath[1][X] = RotorPath[1][X-1];
         }
     RotorPath[1][0] = store;
+    
+    for (X = 0; X < 26; X++)
+        printf("%s", RotorPath[1][X]);
+        printf("\n");
 }
 void Rotor3Spin() // Spins Rotor 3 once
 {
@@ -59,9 +65,11 @@ void Rotor3Spin() // Spins Rotor 3 once
             RotorPath[2][X] = RotorPath[2][X-1];
         }
     RotorPath[2][0] = store;
+    
+    for (X = 0; X < 26; X++)
+        printf("%s", RotorPath[2][X]);
+        printf("\n");
 }
- 
-
  
 int main() 
 {
@@ -77,24 +85,25 @@ int main()
     Message_Length = strlen(Message);
     // Finds length of message
         
-        
-    for (X = 0; X < 26; X++)
-        printf("%s", RotorPath[0][X]);
-        printf("\n");
-    for (X = 0; X < 26; X++)
+   /*******************************************************     
+   for(Z = 0; Z< 26; Z++) 
     {
+        for (Y = 0; Y < 26; Y++)
+        {
+            for (X = 0; X < 26; X++)
+            {
+                Rotor3Spin();
+            }
+            Rotor2Spin();
+            printf("------------------------------\n");
+        }
         Rotor1Spin();
-        printf("\n");
+        printf("*******************************\n");
     }
-    // The Rotor spinning 26 times
+    // The Rotor spinning Thorugh All Combinations times
    
-
+    *********************************************************/
         
-        int Test, Test2;
-    Test = * RotorPath[0][0] - 65;
-    printf("%d\n", Test);
-    Test2 = * RotorPath[1][Test] - 65;
-    printf("%d\n", Test2);
- 
+        
   return 0;
 }
