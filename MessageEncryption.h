1f0beb12b24de789c369d87909da8c8e6c435224
TylerBruno0 <127354973+TylerBruno0@users.noreply.github.com>
2023-03-27 14:35:53 -0700

    Update MessageEncryption.h

--- MessageEncryption.h
+++ MessageEncryption.h
@@ -2,105 +2,70 @@
 *
 *  File: MessageEncryption.h
 *  Author:  Jake Klinger, Jose (Pillo) Duenas-Lopez, Tyler Bruno
-*  Version: 0.90(Mar 26, 2023)
+*  Version: 0.20(Mar 24, 2023)
 *  
 * Purpose: 
 *            Contains the function to encrypt and decrypt messages. 
 *
 *
 *************************************************************/
-
-
 #include <stdio.h>
 #include <string.h>
 #include "RotorMovement.h"
-#include "RotorWiring.h"
 
-void CharacterEncryption(int Character) //Function to send a character through the rotors and back and receive decrypted character
+void CharacterEncryption(int input) //Function to send a character through the rotors and back and receive decrypted character
 {
     int temp;
-    temp = Character;
+    temp = input;
     for (int i = 0; i <= 3; i++)
     {
         temp = * RotorPath[i][temp] - 65;
-        printf("%d\n",temp);
+
     }
-    
-    for (int j = 2; j >= 0; j--) 
+
+    for (int j = 2; j >= 0; j--)
     {
         for(int i = 0; i < 26; i++)
-        {    
+        {
             if(* RotorPath[j][i] - 65 == temp )
             {
                 temp = i;
-                break;    
-            }  
-        }   
-        printf("%d\n",temp);
+                break;
+            }
+        }
+
     }
-    printf("%c\n", temp + 65); 
+    FILE* file=fopen("EnigmaTranscript.txt","a");
+    printf("%c", temp + 65);
+    fprintf(file, "%c", temp + 65);
+
 }
 
-void MessageEncryption()
+void MessageEncryption(char *Message)
 {
-
-    int Message_Length, R3_Change, R2_Change, R1_Change;
-    // Finds length of message
-    char Message[1000];
-    printf("Message: ");
-    scanf("%s" , &Message);
-    Message_Length = strlen(Message);
-    // Values to check how many times the upper rotors spin
-    R1_Change = Message_Length/676;
-    R2_Change = Message_Length/26;
-    if (Message_Length > 26)
-        R3_Change = 25;
-    else 
-        R3_Change = Message_Length;
-        
-        
-    // Runs if Message is long enough the first rotor spins (more than 676 characters)
-   if (R1_Change > 0)
-   {
-       for(int k = 0; k < R1_Change ; k++) 
-       {
-           for (int j = 0; j < R2_Change ; j++)
-           {
-               for (int i = 0; i < R3_Change; i++)
-               {
-                   CharacterEncryption(Message[i]-65);
-                   Rotor3Spin();
-               }
-               Rotor2Spin();
-               
-           }
-           Rotor1Spin();
-           
-       }
-   }
-   // Runs if Message is long enough the second rotor spins (more than 26 characters)
-   if (R2_Change > 0); 
-   {
-       for (int j = 0; j < R2_Change ; j++)
-       {
-           for (int i = 0; i < R3_Change; i++)
-           {
-                CharacterEncryption(Message[i]-65);
-                Rotor3Spin();
-           }
-           Rotor2Spin();
-           
-       }
-   }
-   // Runs if Message is smaller than 26 characters.
-   if (R3_Change < 26)
-   {
-       for (int i = 0; i < R3_Change; i++)
-       {
-           CharacterEncryption(Message[i]-65);
-           Rotor3Spin();
-       }
-   }
-   printf("\n");
+    int Message_Length = 0, Count1 = 0, Count2 = 0;
+    // Finds Length of the input message
+    for(int i = 0; Message[i] != '\0'; i++)
+    {
+        Message_Length++;
+    }
+    //Encrypts or Decrypts the message the user input
+    for(int i = 0; i < Message_Length; i++)
+    {
+        CharacterEncryption(Message[i] - 65);
+        Rotor3Spin();
+        Count1++;
+        if (Count1 == 26)
+        {
+            Count1 == 0;
+            Rotor2Spin();
+            Count2 ++;
+        }
+        if (Count2 == 26)
+        {
+            Count2 == 0;
+            Rotor1Spin();
+        }
+    }
+    printf("\n");
 }
-
